d285bbdc23aabe8355afcc643dcbad6a537ab198
MarecoAJ <alejo_mareco@hotmail.com>
2024-05-17 21:48:26 -0300

    creacion de data-set del db

---
+++ data-set.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE `db_finanzasPersonales`;
+
+INSERT INTO `tipo_Movimientos` (`descrip_tipo_mov`) VALUES ('INGRESOS');
+INSERT INTO `tipo_Movimientos` (`descrip_tipo_mov`) VALUES ('EGRESOS');
