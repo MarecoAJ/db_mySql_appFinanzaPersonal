c549df29a5857bd2b264cfb530927281e6105a56
MarecoAJ <alejo_mareco@hotmail.com>
2024-05-28 21:40:56 -0300

    correcciones

--- data-set.sql
+++ data-set.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE `db_finanzasPersonales`;
 
-INSERT INTO `tipo_Movimientos` (`descrip_tipo_mov`) VALUES ('INGRESOS');
-INSERT INTO `tipo_Movimientos` (`descrip_tipo_mov`) VALUES ('EGRESOS');
+INSERT INTO `tipos_Movimientos` (`descrip_tipo_mov`) VALUES ('INGRESOS');
+INSERT INTO `tipos_Movimientos` (`descrip_tipo_mov`) VALUES ('EGRESOS');
